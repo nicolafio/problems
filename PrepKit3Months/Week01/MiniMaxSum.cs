7cbc8f00a7411abe261d33fb9585c93b3f26849f
Nicola Fiori (JD342) <nicola@jd342.net>
2024-01-08 18:35:37 +0000

    Solve second problem

--- PrepKit3Months/Week01/MiniMaxSum.cs
+++ PrepKit3Months/Week01/MiniMaxSum.cs
@@ -1,6 +1,47 @@
-namespace PrepKit3Months.Week01;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-public class MinMaxSum
+class Result
 {
-    
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        long sum = arr.Sum(v => (long)v);
+        long min = arr.Min();
+        long max = arr.Max();
+        long maxSum = sum - min;
+        long minSum = sum - max;
+        Console.Write(minSum);
+        Console.Write(" ");
+        Console.WriteLine(maxSum);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
 }
