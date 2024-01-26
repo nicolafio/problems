fb6e32541a789809b4e76240ed0f44f3bf687674
Nicola Fiori (JD342) <nicola@jd342.net>
2024-01-26 22:21:08 +0000

    Solve challenge

--- DivisibleSumPairs/Solution.cs
+++ DivisibleSumPairs/Solution.cs
@@ -27,7 +27,12 @@ class Result
 
     public static int divisibleSumPairs(int n, int k, List<int> ar)
     {
-
+        var count = 0;
+        for (var i = 0; i < n; i++)
+            for (var j = i + 1; j < n; j++)
+                if ((ar[i] + ar[j]) % k == 0)
+                    count++;
+        return count;
     }
 
 }
