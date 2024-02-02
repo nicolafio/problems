d0277db0cfc442d272fb954cef02b40e63eeba9f
Nicola Fiori (JD342) <nicola@jd342.net>
2024-02-02 12:36:11 +0000

    Tweak

--- choose.js
+++ choose.js
@@ -1,17 +1,35 @@
 const SOURCES = {
     HR_3MOPK: 'https://www.hackerrank.com/interview/preparation-kits/three-month-preparation-kit/',
-    HR_A: 'https://www.hackerrank.com/domains/algorithms',
-    HR_DS: 'https://www.hackerrank.com/domains/data-structures',
+    HR_ALGOS_EASY: 'https://www.hackerrank.com/domains/algorithms?filters%5Bdifficulty%5D%5B%5D=easy',
+    HR_ALGOS_MEDIUM: 'https://www.hackerrank.com/domains/algorithms?filters%5Bdifficulty%5D%5B%5D=medium',
+    HR_ALGOS_HARD: 'https://www.hackerrank.com/domains/algorithms?filters%5Bdifficulty%5D%5B%5D=HARD',
+    HR_DS_EASY: 'https://www.hackerrank.com/domains/data-structures?filters%5Bdifficulty%5D%5B%5D=easy',
+    HR_DS_MEDIUM: 'https://www.hackerrank.com/domains/data-structures?filters%5Bdifficulty%5D%5B%5D=medium',
+    HR_DS_HARD: 'https://www.hackerrank.com/domains/data-structures?filters%5Bdifficulty%5D%5B%5D=hard',
     ICPC: 'https://icpc.global/worldfinals/past-problems',
 }
+
+const DIFFICULTY = {
+    EASY: 'Easy',
+    MEDIUM: 'Medium',
+    HARD: 'Hard'
+};
 
 function pickRandomSource() {
     if (Math.random() < .8) {
         if (Math.random() < .8) return SOURCES.HR_3MOPK;
-        if (Math.random < .8) return SOURCES.HR_A;
+        const difficulty = pickRandomDifficulty();
+        if (Math.random < .8) {
+            if (difficulty === DIFFICULTY.EASY) return SOURCES.HR_ALGOS_EASY;
+            if (difficulty === DIFFICULTY.MEDIUM) return SOURCES.HR_ALGOS_MEDIUM;
+            if (difficulty === DIFFICULTY.HARD) return SOURCES.HR_ALGOS_HARD;
+        }
+        if (difficulty === DIFFICULTY.EASY) return SOURCES.HR_DS_EASY;
+        if (difficulty === DIFFICULTY.MEDIUM) return SOURCES.HR_DS_MEDIUM;
+        if (difficulty === DIFFICULTY.HARD) return SOURCES.HR_DS_HARD;
         return SOURCES.HR_DS;
     };
-    return SOURCES.ICPC;    
+    return SOURCES.ICPC;
 }
 
 function pickRandomLanguage(source) {
@@ -23,9 +41,9 @@ function pickRandomLanguage(source) {
 }
 
 function pickRandomDifficulty() {
-    if (Math.random() < .4) return 'Easy';
-    if (Math.random() < .6) return 'Medium';
-    return 'Hard';
+    if (Math.random() < .4) return DIFFICULTY.EASY;
+    if (Math.random() < .6) return DIFFICULTY.MEDIUM;
+    return DIFFICULTY.HARD;
 }
 
 const source = pickRandomSource();
@@ -34,7 +52,3 @@ const language = pickRandomLanguage(source);
 console.log(`Source: ${source}`);
 console.log(`Language: ${language}`);
 
-if ([SOURCES.HR_A, SOURCES.HR_DS].includes(source)) {
-    const difficulty = pickRandomDifficulty();
-    console.log(`Difficulty: ${difficulty}`);
-}
