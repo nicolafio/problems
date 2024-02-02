d03c797f469f314742a26cec27471159550a1508
Nicola Fiori (JD342) <nicola@jd342.net>
2024-02-02 14:04:47 +0000

    Tweak and expand

--- choose.js
+++ choose.js
@@ -1,54 +1,125 @@
-const SOURCES = {
-    HR_3MOPK: 'https://www.hackerrank.com/interview/preparation-kits/three-month-preparation-kit/',
-    HR_ALGOS_EASY: 'https://www.hackerrank.com/domains/algorithms?filters%5Bdifficulty%5D%5B%5D=easy',
-    HR_ALGOS_MEDIUM: 'https://www.hackerrank.com/domains/algorithms?filters%5Bdifficulty%5D%5B%5D=medium',
-    HR_ALGOS_HARD: 'https://www.hackerrank.com/domains/algorithms?filters%5Bdifficulty%5D%5B%5D=HARD',
-    HR_DS_EASY: 'https://www.hackerrank.com/domains/data-structures?filters%5Bdifficulty%5D%5B%5D=easy',
-    HR_DS_MEDIUM: 'https://www.hackerrank.com/domains/data-structures?filters%5Bdifficulty%5D%5B%5D=medium',
-    HR_DS_HARD: 'https://www.hackerrank.com/domains/data-structures?filters%5Bdifficulty%5D%5B%5D=hard',
-    ICPC: 'https://icpc.global/worldfinals/past-problems',
-}
 
-const DIFFICULTY = {
-    EASY: 'Easy',
-    MEDIUM: 'Medium',
-    HARD: 'Hard'
-};
+const ICPC_LATEST_YEAR = 2022;
 
 function pickRandomSource() {
+
+    // HackerRank
+
     if (Math.random() < .8) {
-        if (Math.random() < .8) return SOURCES.HR_3MOPK;
-        const difficulty = pickRandomDifficulty();
-        if (Math.random < .8) {
-            if (difficulty === DIFFICULTY.EASY) return SOURCES.HR_ALGOS_EASY;
-            if (difficulty === DIFFICULTY.MEDIUM) return SOURCES.HR_ALGOS_MEDIUM;
-            if (difficulty === DIFFICULTY.HARD) return SOURCES.HR_ALGOS_HARD;
+        if (Math.random() < .8)
+            return 'https://www.hackerrank.com/interview/preparation-kits/three-month-preparation-kit/';
+
+        let difficulty = 'easy';
+        if (Math.random() < .6) {
+            difficulty = 'medium';
+            if (Math.random() < .2) {
+                difficulty = 'hard';
+            } 
         }
-        if (difficulty === DIFFICULTY.EASY) return SOURCES.HR_DS_EASY;
-        if (difficulty === DIFFICULTY.MEDIUM) return SOURCES.HR_DS_MEDIUM;
-        if (difficulty === DIFFICULTY.HARD) return SOURCES.HR_DS_HARD;
-        return SOURCES.HR_DS;
+
+        let domain = 'algorithms';
+        if (Math.random() < .2) {
+            domain = 'data-structures';
+        }
+
+        return `https://www.hackerrank.com/domains/${domain}?filters%5Bdifficulty%5D%5B%5D=${difficulty}`;
     };
-    return SOURCES.ICPC;
+
+    // ICPC
+
+    if (Math.random() < .2)
+        return 'https://icpc.global/worldfinals/past-problems';
+
+    if (Math.random() < .6) {
+
+        // North America & Europe
+
+        if (Math.random() < .5) {
+            console.log('Todo: choices to be expanded');
+            return 'https://icpc.global/regionals/finder/na';
+        }
+
+        // https://icpc.global/regionals/finder/eu
+
+        const region = pickRandomItem(['NW', 'SW', 'CE', 'SE', 'NE']);
+
+        if (region === 'NW') {
+            console.log('Todo: choices to be expanded');
+            return 'https://icpc.global/regionals/finder/northwestern-europe';
+        }
+
+        if (region === 'SW') {
+            console.log('Todo: choices to be expanded');
+            return 'https://icpc.global/regionals/finder/swerc';
+        }
+
+        if (region === 'CE') {
+            console.log('Todo: choices to be expanded');
+            return 'https://icpc.global/regionals/finder/central-europe';
+        }
+
+        if (region === 'SE') {
+            // https://icpc.global/regionals/finder/seerc
+            // http://acm.ro/past.htm
+
+            let years = new Set([...range(1995, ICPC_LATEST_YEAR - 1)]);
+
+            years.delete(1997); // No problems published there.
+
+            years = [...years];
+
+            const year = pickRandomItem(years);
+
+            if (year === 2019) return 'http://acm.ro/2019/prob/probleme2019acm.pdf';                
+
+            console.log('Todo: choices to be expanded');
+            return `http://acm.ro/${year}/problems.htm`;
+        }
+
+        if (region === 'NE') {
+            console.log('Todo: choices to be expanded');
+            return 'https://icpc.global/regionals/finder/northern-eurasia';
+        }
+
+    }
+
+    if (Math.random() < .6) {
+        // ICPC Russia & China...
+        console.log('Todo: choices to be expanded for Russia & China');
+        return 'https://icpc.global/regionals/finder';
+    }
+
+    // ICPC - rest of the world...
+    console.log('Todo: choices to be expanded for regionals that are not North America, Europe, Russia, China');
+    return 'https://icpc.global/regionals/finder';
 }
 
 function pickRandomLanguage(source) {
-    if (Math.random() < .6 && source != SOURCES.ICPC) return 'C#';
+    if (Math.random() < .6) return 'C#';
     if (Math.random() < .6) return 'C++';
     if (Math.random() < .6) return 'C';
-    if (Math.random() < .6 && source != SOURCES.ICPC) return 'JavaScript';
+    if (Math.random() < .6) return 'JavaScript';
     return 'Python';
 }
 
-function pickRandomDifficulty() {
-    if (Math.random() < .4) return DIFFICULTY.EASY;
-    if (Math.random() < .6) return DIFFICULTY.MEDIUM;
-    return DIFFICULTY.HARD;
+function pickRandomProblem(source) {
+    if (source.includes('http://acm.ro/2019'))
+        return pickRandomItem('ABCDEFGHIJK');
+}
+
+function pickRandomItem(items) {
+    return items[Math.floor(Math.random() * items.length)];
+}
+
+function* range(min, max, increment = 1) {
+    for (let i = min; i <= max; i += increment) yield i;
 }
 
 const source = pickRandomSource();
 const language = pickRandomLanguage(source);
+const problem = pickRandomProblem(source);
 
 console.log(`Source: ${source}`);
 console.log(`Language: ${language}`);
 
+if (problem) console.log(`Problem: ${problem}`);
