4ea98a039563c58786558f0de083e7787b16643f
Nicola Fiori (JD342) <nicola@jd342.net>
2024-02-12 15:09:15 +0000

    Solve

--- hackerrank/magic-square-forming/solution.cpp
+++ hackerrank/magic-square-forming/solution.cpp
@@ -8,15 +8,61 @@ string ltrim(const string &);
 string rtrim(const string &);
 vector<string> split(const string &);
 
+bool isMagicSquare(vector<int> t) {
+    int magicNumber = t[0] + t[1] + t[2];
+
+    // Check diagonals.
+    if ((t[0] + t[4] + t[8]) != magicNumber) return false;
+    if ((t[2] + t[4] + t[6]) != magicNumber) return false;
+
+    // Check rows and columns.
+    for (int i = 0; i < 3; i++) {
+        if ((t[3 * i] + t[3 * i + 1] + t[3 * i + 2]) != magicNumber) return false;
+        if ((t[i] + t[3 + i] + t[6 + i]) != magicNumber) return false;  
+    }
+
+    return true;
+}
+
+int findCost(vector<int> q, vector<int> t) {
+    int cost = 0;
+    for (int i = 0; i < 9; i++) {
+        cost += abs(q[i] - t[i]);
+    }
+    return cost;
+}
+
 /*
  * Complete the 'formingMagicSquare' function below.
  *
  * The function is expected to return an INTEGER.
  * The function accepts 2D_INTEGER_ARRAY s as parameter.
  */
-
 int formingMagicSquare(vector<vector<int>> s) {
-    
+    // Brute force solution.
+
+    int minimumCost = INFINITY;
+
+    vector<int> t{
+        1, 2, 3,
+        4, 5, 6,
+        7, 8, 9
+    };
+
+    vector<int> q{
+        s[0][0], s[0][1], s[0][2],
+        s[1][0], s[1][1], s[1][2],
+        s[2][0], s[2][1], s[2][2]
+    };
+
+    while (next_permutation(t.begin(), t.end())) {
+        if (isMagicSquare(t)) {
+            int cost = findCost(q, t);
+            if (cost < minimumCost) minimumCost = cost;
+        }
+    }
+
+    return minimumCost;
 }
 
 int main()
