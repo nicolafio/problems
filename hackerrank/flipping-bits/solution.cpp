fad3b237b78f54792114625dce8aa70c520b844f
Nicola Fiori (JD342) <nicola@jd342.net>
2024-02-12 22:48:56 +0000

    Fix compiler errors
    Doing the same changes as for Magic Square Forming

--- hackerrank/flipping-bits/solution.cpp
+++ hackerrank/flipping-bits/solution.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
@@ -46,7 +48,7 @@ string ltrim(const string &str) {
 
     s.erase(
         s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+        find_if(s.begin(), s.end(), [](int c) {return !isspace(c);})
     );
 
     return s;
@@ -56,7 +58,7 @@ string rtrim(const string &str) {
     string s(str);
 
     s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        find_if(s.rbegin(), s.rend(), [](int c) {return !isspace(c);}).base(),
         s.end()
     );
 
