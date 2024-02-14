e155814d112758ac0b8a158c8347753745d72f84
Nicola Fiori (JD342) <nicola@jd342.net>
2024-02-14 09:31:49 +0000

    Solve (with conjecture, not sure why it works)

--- hackerrank/flipping-the-matrix/solution.py
+++ hackerrank/flipping-the-matrix/solution.py
@@ -17,34 +17,17 @@ import sys
 
 def flippingMatrix(matrix):
     # Write your code here
-    # Greedy approach
     n = len(matrix)
-    firstHalf = range(int(n / 2))
-    secondHalf = range(n - int(n / 2), n)
-    settled = False
-    i = 0
-    orientation = 0
-    index = 0
-    while not settled:
-        settled = True
-        for i in range(n):
-            leftRowSum =        sum([matrix[i][j] for j in firstHalf])
-            rightRowSum =       sum([matrix[i][j] for j in secondHalf])
-            if rightRowSum > leftRowSum:
-                settled = False
-                flippedRow = [matrix[i][n - j - 1] for j in range(n)]
-                for j in range(n):
-                    matrix[i][j] = flippedRow[j]
 
-            topColumnSum =      sum([matrix[j][i] for j in firstHalf])
-            bottomColumnSum =   sum([matrix[j][i] for j in secondHalf])
-            if bottomColumnSum > topColumnSum:
-                settled = False
-                flippedColumn = [matrix[n - j - 1][i] for j in range(n)]
-                for j in range(n):
-                    matrix[j][i] = flippedColumn[j]
-
-    return sum([matrix[i][j] for i in firstHalf for j in firstHalf])
+    return sum([
+        max([
+            matrix[k][l]
+            for k in [i, n - i - 1]
+            for l in [j, n - j - 1]
+        ])
+        for i in range(int(n / 2))
+        for j in range(int(n / 2))
+    ])
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
