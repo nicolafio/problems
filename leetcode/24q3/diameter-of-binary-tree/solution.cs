17e4379f62439a77680a5ca09f32ce8890edbd66
Nicola Fiori <nicola@jd342.net>
2024-08-04 13:54:33 +0100

    First attempt (wrong)

--- leetcode/24q3/diameter-of-binary-tree/solution.cs
+++ leetcode/24q3/diameter-of-binary-tree/solution.cs
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     public int val;
@@ -13,6 +13,65 @@
  */
 public class Solution {
     public int DiameterOfBinaryTree(TreeNode root) {
-        
+        var parentMemo = new Dictionary<TreeNode, TreeNode?>(); 
+        var rightDepthMemo = new Dictionary<TreeNode, int>();
+        var leftDepthMemo = new Dictionary<TreeNode, int>();
+        var downDepthMemo = new Dictionary<TreeNode, int>();
+
+        parentMemo[root] = null;
+
+        recordParents(root, parentMemo);
+        recordDepth(root, downDepthMemo, (n) => new(){n.left, n.right});
+        recordDepth(root, rightDepthMemo, (n) => new(){n.right, parentMemo[n]});
+        recordDepth(root, leftDepthMemo, (n) => new(){n.left, parentMemo[n]});
+
+        var depthMemos = new List<Dictionary<TreeNode, int>>() {
+            rightDepthMemo,
+            leftDepthMemo,
+            downDepthMemo
+        };
+
+        int diameter = 0;
+
+        foreach (var depthMemo in depthMemos) {
+            foreach (var depth in depthMemo.Values) {
+                if (depth > diameter) {
+                    diameter = depth;
+                }
+            }
+        }
+
+        return diameter;
+    }
+
+    private void recordParents(
+        TreeNode node,
+        Dictionary<TreeNode, TreeNode?> parentMemo
+    ) {
+        foreach (var n in new List<TreeNode>{node.left, node.right}) {
+            if (n != null) {
+                parentMemo[n] = node;
+                recordParents(n, parentMemo);
+            }
+        }
+    }
+    
+    private void recordDepth(
+        TreeNode node,
+        Dictionary<TreeNode, int> depthMemo,
+        Func<TreeNode, List<TreeNode?>> getNext
+    ) {
+        if (depthMemo.ContainsKey(node)) return;
+
+        int depth = 0;
+
+        foreach (var next in getNext(node)) {
+            if (next != null) {
+                recordDepth(next, depthMemo, getNext);
+                depth += 1 + depthMemo[next];
+            }
+        }
+
+        depthMemo[node] = depth;
     }
 }
