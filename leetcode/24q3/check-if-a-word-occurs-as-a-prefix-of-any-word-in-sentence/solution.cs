6cbeb066ca3935eb731220e6afd045731a04e583
Nicola Fiori <nicola@jd342.net>
2024-08-05 19:52:47 +0100

    Solve

--- leetcode/24q3/check-if-a-word-occurs-as-a-prefix-of-any-word-in-sentence/solution.cs
+++ leetcode/24q3/check-if-a-word-occurs-as-a-prefix-of-any-word-in-sentence/solution.cs
@@ -1,5 +1,13 @@
 ﻿public class Solution {
     public int IsPrefixOfWord(string sentence, string searchWord) {
-        
+        var words = sentence.Split(" ");
+        for (int i = 0; i < words.Length; i++) {
+            var word = words[i];
+            if (word.Length < searchWord.Length) continue;
+            if (word.Substring(0, searchWord.Length) == searchWord) {
+                return i + 1;
+            }
+        }        
+        return -1;
     }
 }
