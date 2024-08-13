6b4ff7638a58e21adfcbd9556ccea5cce4700679
Nicola Fiori <nicola@jd342.net>
2024-08-13 07:32:41 +0100

    Solve

--- boj/24q3/baekjoon/test-scores/solution.cc
+++ boj/24q3/baekjoon/test-scores/solution.cc
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-    cout << "Implement me." << endl;
+    int score;
+    string grade;
+
+    cin >> score;
+
+    if (score >= 90 && score <= 100)
+        grade = "A";
+    else if (score >= 80)
+        grade = "B";
+    else if (score >= 70)
+        grade = "C";
+    else if (score >= 60)
+        grade = "D";
+    else
+        grade = "F";
+
+    cout << grade << endl;
 }
