00f8db01552fcf355f5e792637c90d818c6295fe
Nicola Fiori <nicola@jd342.net>
2024-08-14 20:27:37 +0100

    Solve

--- icpc/24q3/ser2009/which-is-greater/solution.cc
+++ icpc/24q3/ser2009/which-is-greater/solution.cc
@@ -3,5 +3,13 @@
 using namespace std;
 
 int main() {
-    cout << "Implement me." << endl;
+    while (true) {
+        int a, b;
+        cin >> a >> b;
+        
+        if (a == 0 && b == 0) break;
+        
+        if (a > b) cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
 }
