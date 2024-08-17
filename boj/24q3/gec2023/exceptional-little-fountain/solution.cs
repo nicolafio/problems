7c679ceb9439ea33f299e1009ae9db0460dfdfb7
Nicola Fiori <nicola@jd342.net>
2024-08-17 20:44:03 +0100

    Solve

--- boj/24q3/gec2023/exceptional-little-fountain/solution.cs
+++ boj/24q3/gec2023/exceptional-little-fountain/solution.cs
@@ -1,6 +1,26 @@
 ﻿namespace Baekjoon {
     class Program {
         static void Main() {
+            uint[] input =
+                Console.ReadLine()
+                    .Split(' ')
+                    .Select(s => uint.Parse(s))
+                    .ToArray();
+
+            uint x0 = input[0];
+            uint N = input[1];
+            uint xn = x0;
+
+            for (uint n = 1; n <= N; n++) {
+                if (xn % 2 == 0) {
+                    xn = (xn / 2) ^ 6;
+                }
+                else {
+                    xn = (xn * 2) ^ 6;
+                }
+            }
+
+            Console.WriteLine(xn);
         }
     }
 }
