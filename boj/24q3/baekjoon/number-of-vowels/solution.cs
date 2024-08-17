a4aa87c613da8614245eac09634bd550a7907c9c
Nicola Fiori <nicola@jd342.net>
2024-08-17 19:39:49 +0100

    Solve

--- boj/24q3/baekjoon/number-of-vowels/solution.cs
+++ boj/24q3/baekjoon/number-of-vowels/solution.cs
@@ -1,6 +1,19 @@
 ﻿namespace Baekjoon {
     class Program {
+        private static readonly HashSet<char> vowels =
+            new HashSet<char>{'a', 'e', 'i', 'o', 'u'};
+
         static void Main() {
+            string line = Console.ReadLine();
+            int count = 0;
+
+            foreach (char c in line) {
+                if (vowels.Contains(c)) {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
         }
     }
 }
