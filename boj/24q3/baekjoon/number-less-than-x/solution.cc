6426f1f42e076d9be03d9a176a26dd1c4d15bacd
Nicola Fiori <nicola@jd342.net>
2024-08-17 19:16:36 +0100

    Solve
    Ref https://www.acmicpc.net/source/82627869

--- boj/24q3/baekjoon/number-less-than-x/solution.cc
+++ boj/24q3/baekjoon/number-less-than-x/solution.cc
@@ -3,5 +3,29 @@
 using namespace std;
 
 int main() {
-    cout << "Implement me." << endl;
+    int N, X;
+    bool first = true;    
+
+    cin >> N >> X;
+
+
+    for (int i = 0; i < N; i++) {
+        int a;
+
+        cin >> a;
+
+        if (a < X) {
+            if (!first) {
+                cout << " ";
+            }
+
+            cout << a;
+
+            first = false;
+        }
+    }
+
+    cout << endl;
+
+    return 0;
 }
