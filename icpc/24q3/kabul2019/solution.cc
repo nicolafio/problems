d9f273d07196023b43295131e4d64d6288f5ac7f
Nicola Fiori <nicola@jd342.net>
2024-08-21 08:56:00 +0100

    Solve

--- icpc/24q3/kabul2019/solution.cc
+++ icpc/24q3/kabul2019/solution.cc
@@ -3,5 +3,19 @@
 using namespace std;
 
 int main() {
-    cout << "Implement me." << endl;
+    int T, N;
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        cin >> N;
+
+        for (int j = 0; j < N; j++) {
+            int a, b;
+
+            cin >> a >> b;
+
+            cout << (a + b) << " " << (a * b) << endl;
+        }
+    }
 }
