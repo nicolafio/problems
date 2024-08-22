d7239a03f3713bc09736aa3c4c96b437cfbaba00
Nicola Fiori <nicola@jd342.net>
2024-08-22 20:48:30 +0100

    Solve

--- icpc/24q3/joi2024/hamming-distance/solution.cc
+++ icpc/24q3/joi2024/hamming-distance/solution.cc
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-    cout << "Implement me." << endl;
+    uint N;
+    string S, T;
+    int c = 0;
+
+    cin >> N >> S >> T;
+
+    for (uint i = 0; i < N; i++) {
+        if (S[i] != T[i]) {
+            c++;
+        }
+    }
+
+    cout << c << endl;
 }
