3d86c3cc316fafdf27310bff8728f8dba7061564
Nicola Fiori (JD342) <nicola@jd342.net>
2024-10-07 21:32:10 +0100

    Solve

--- icpc/24q4/ccc2012j/icon-scaling/solution.cc
+++ icpc/24q4/ccc2012j/icon-scaling/solution.cc
@@ -2,6 +2,23 @@
 
 using namespace std;
 
+char icon[3][3] = {
+    {'*', 'x', '*'},
+    {' ', 'x', 'x'},
+    {'*', ' ', '*'}
+};
+
 int main() {
-    cout << "Implement me." << endl;
+    int k;
+    cin >> k;
+    for (int i = 0; i < 3; i++) {
+        for (int ii = 0; ii < k; ii ++) {
+            for (int j = 0; j < 3; j++) {
+                for (int jj = 0; jj < k; jj++) {
+                    cout << icon[i][j];
+                }
+            }
+            cout << endl;
+        }
+    }
 }
