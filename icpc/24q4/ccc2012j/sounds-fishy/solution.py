0d4f2962ad56dcee59064044b2edee4e432fd895
Nicola Fiori (JD342) <nicola@jd342.net>
2024-10-07 21:17:27 +0100

    Solve

--- icpc/24q4/ccc2012j/sounds-fishy/solution.py
+++ icpc/24q4/ccc2012j/sounds-fishy/solution.py
@@ -1,1 +1,33 @@
-print("Implement me")
+def _main():
+    readings = [int(input()) for _ in range(4)]
+    if _is_stricly_increasing(readings):
+        print("Fish Rising")
+    elif _is_strictly_decreasing(readings):
+        print("Fish Diving")
+    elif _is_identical(readings):
+        print("Fish At Constant Depth")
+    else:
+        print("No Fish")
+
+def _is_stricly_increasing(readings):
+    for a, b in _pairs(readings):
+        if a >= b:
+            return False
+    return True
+
+def _is_strictly_decreasing(readings):
+    for a, b in _pairs(readings):
+        if a <= b:
+            return False
+    return True
+
+def _is_identical(readings):
+    for a, b in _pairs(readings):
+        if a != b:
+            return False
+    return True
+
+def _pairs(readings):
+    return [(a, b) for a, b in zip(readings, readings[1:])]
+
+_main()
