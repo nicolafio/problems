495bd8a6eca93c103625e1c6bdf2a2ebf7fbfb16
Nicola Fiori (JD342) <nicola@jd342.net>
2024-10-13 19:45:19 +0100

    Solve

--- icpc/24q4/ccc2012j/big-bang-secrets/solution.cc
+++ icpc/24q4/ccc2012j/big-bang-secrets/solution.cc
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
 
 using namespace std;
 
+vector<char> chars({
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+});
+
 int main() {
-    cout << "Implement me." << endl;
+    int K;
+    string word;
+
+    cin >> K >> word;
+
+    unordered_map<char, int> charIndex;
+
+    for (int i = 0; i < chars.size(); i++) {
+        charIndex.emplace(chars[i], i);
+    }
+
+    for (int i = 0; i < word.size(); i++) {
+        char encoded = word[i];
+        char P = i + 1;
+        char S = 3 * P + K;
+
+        int j = charIndex[encoded];
+        int k = (j + chars.size() - S) % chars.size();
+        char decoded = chars[k];
+
+        cout << decoded;
+    }
+
+    cout << endl;
+
+    return 0;
 }
