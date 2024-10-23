b1afad34b902e37b5950eaa5f79b2bed6f948f33
Nicola Fiori (JD342) <nicola@jd342.net>
2024-10-23 22:47:45 +0100

    First attempt (TLE)

--- icpc/24q4/bapc2012p/annoying-mosquitos/solution.cc
+++ icpc/24q4/bapc2012p/annoying-mosquitos/solution.cc
@@ -1,10 +1,70 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+void runCase() {
+    int n;
+
+    cin >> n;
+
+    int x[n];
+    int y[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+    }
+
+    int m;
+
+    cin >> m;
+
+    int minCoord = -1000;
+    int maxCoord = 1000;
+    int coordinatesCount = abs(minCoord) + maxCoord + 1;
+
+    bool hit[coordinatesCount][coordinatesCount];
+
+    for (int i = 0; i < coordinatesCount; i++)
+        for (int j = 0; j < coordinatesCount; j++)
+            hit[i][j] = false;
+
+    for (int j = 0; j < m; j++) {
+        int ax, ay;
+        cin >> ax >> ay;
+
+        for (int i = 0; i <= 50; i++) {
+            for (int ii = 0; ii <= 50; ii++) {
+                for (int k = -1; k <= 1; k += 2) {
+                    for (int kk = -1; kk <= 1; kk += 2) {
+                        int hitX = ax + (i * k);
+                        int hitY = ay + (ii * kk);
+                        if (hitX < -1000 || hitX > 1000) continue;
+                        if (hitY < -1000 || hitY > 1000) continue;
+                        hit[hitX + abs(minCoord)][hitY + abs(minCoord)] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    int hits = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (hit[x[i] + abs(minCoord)][y[i] + abs(minCoord)])
+            hits++;
+    }
+
+    cout << hits << endl;
+}
+
 int main() {
-    string input;
-    cin >> input;
-    cout << input;
+    int casesCount;
+
+    cin >> casesCount;
+
+    for (int i = 0; i < casesCount; i++) {
+        runCase();
+    }
 }
