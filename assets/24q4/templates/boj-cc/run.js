03032baf58c64a7076be04414cc345326ffc1889
Nicola Fiori (JD342) <nicola@jd342.net>
2024-10-23 22:00:01 +0100

    Squashed commit of the following:
    commit 36c078f4c108c2eedaf0fc5c6ee392f29389444c
    Author: Nicola Fiori (JD342) <nicola@jd342.net>
    Date:   Wed Oct 23 21:51:23 2024 +0100
    
        Fix logic
    
    commit 19acfd721cacd84eb040c9af9681cf76eecd263f
    Author: Nicola Fiori (JD342) <nicola@jd342.net>
    Date:   Wed Oct 23 10:57:43 2024 +0200
    
        Implement clean-up logic with graceful termination
    
    commit 65dfcff67625f2f7aaf7fec8cf680c328339bb27
    Author: Nicola Fiori (JD342) <nicola@jd342.net>
    Date:   Wed Oct 23 10:11:34 2024 +0200
    
        Remove unneccessary import
    
    commit 96b34fb5b8797615289ba0f35acb1972e503a206
    Author: Nicola Fiori (JD342) <nicola@jd342.net>
    Date:   Tue Oct 22 15:53:18 2024 +0200
    
        Watch logic

--- assets/24q4/templates/boj-cc/run.js
+++ assets/24q4/templates/boj-cc/run.js
@@ -1,7 +1,6 @@
 const { spawn, spawnSync } = require('child_process');
 const { basename, extname } = require('path');
 const { readdirSync, readFileSync } = require('fs');
-const { Readable } = require('stream');
 
 const DOCKERFILE = `${__dirname}/build.Dockerfile`;
 
@@ -9,10 +8,26 @@ main().catch(console.error);
 
 async function main() {
     const problem = basename(__dirname);
+    const sourcePath = __dirname + '/solution.cc';
     const tag = `${problem}-build`;
+    let terminating = false;
 
-    if (build(tag).status === 0)
-        await run(tag);
+    const onTermination = () => { terminating = true };
+
+    process.on('SIGINT', onTermination);
+    process.on('SIGTERM', onTermination);
+
+    while (!terminating) {
+        const hash = getFileHash(sourcePath);
+
+        if (build(tag).status === 0)
+            await run(tag);
+
+        while (!terminating && getFileHash(sourcePath) === hash)
+            await wait(15);
+    }
+
+    clean(tag);
 }
 
 function build(tag) {
@@ -37,45 +52,83 @@ async function run(tag) {
     try { dirEntries = readdirSync(inputDir) }
     catch (_) {}
 
-    if (dirEntries === null || argsInclude('i', 'interact')) {
+    const interactive = dirEntries === null || argsInclude('i', 'interact');
+
+    const toClean = [];
+
+    if (interactive) {
         console.log('Running interactively');
 
-        const cmd = 'docker';
-        const args = ['run', '-it', tag, '/work/solution'];
-        return spawnSync(cmd, args, { stdio: 'inherit' });
+        toClean.push(tag);
+
+        spawnSync('docker', [
+            'run',
+            '--interactive',
+            '--tty',
+            '--name', tag,
+            tag,
+            '/work/solution'
+        ], { stdio: 'inherit' });
     }
 
-    for (const entry of dirEntries) {
-        if (extname(entry) != '.txt') continue;
+    if (!interactive) {
+        for (const entry of dirEntries) {
+            const suffix = extname(entry);
 
-        const path = `${inputDir}/${entry}`;
-        const txt = readFileSync(path, 'utf-8');
+            if (suffix != '.txt') continue;
 
-        console.log(`-------${'-'.repeat(entry.length)}-`);
-        console.log(`INPUT (${entry})`);
-        console.log(`-------${'-'.repeat(entry.length)}-`);
-        console.log(txt);
+            const name = `${tag}-${basename(entry,suffix)}`;
+            const path = `${inputDir}/${entry}`;
+            const txt = readFileSync(path, 'utf-8');
 
-        console.log('------');
-        console.log('OUTPUT');
-        console.log('------');
+            console.log(`\n\n-------${'-'.repeat(entry.length)}-`);
+            console.log(`INPUT (${entry})`);
+            console.log(`-------${'-'.repeat(entry.length)}-`);
+            console.log(txt);
 
-        const cmd = 'docker';
-        const args = ['run', '-i', tag, '/work/solution'];
-        const child = spawn(cmd, args, {
-            stdio: ['pipe', 'inherit', 'inherit'],
-        });
+            console.log('------');
+            console.log('OUTPUT');
+            console.log('------');
 
-        child.stdin.write(txt);
+            toClean.push(name);
 
-        await new Promise((resolve) => {
-            child.on('exit', () => {
-                resolve();
-            })
-        });
+            const child = spawn('docker', [
+                'run',
+                '--interactive',
+                '--name', name,
+                tag,
+                '/work/solution'
+            ], {
+                stdio: ['pipe', 'inherit', 'inherit'],
+            });
+
+            child.stdin.write(txt);
+
+            await new Promise((resolve) => {
+                child.on('exit', () => {
+                    resolve();
+                })
+            });
+
+            console.log();
+        }
     }
+
+    console.log('\nCleaning up containers');
+
+    for (const name of toClean)
+        spawnSync('docker', ['rm', '--volumes', name], {
+            stdio: ['inherit', 'inherit', 'inherit']
+        });
 }
 
+function clean(tag) {
+    console.log('\nCleaning up image');
+
+    spawnSync('docker', ['image', 'remove', '-f', tag], {
+        stdio: ['inherit', 'inherit', 'inherit']
+    });
+}
 
 function argsInclude(...items) {
     for (const item of items) {
@@ -86,4 +139,20 @@ function argsInclude(...items) {
         }
     }
     return false;
+}
+
+function getFileHash(path) {
+    const md5 = spawnSync('md5', ['-q', path], {
+        encoding: 'utf-8',
+        stdio: ['pipe', 'pipe', 'inherit']
+    });
+    const hash = md5.stdout;
+
+    return hash;
+}
+
+async function wait(milliseconds) {
+    return new Promise((res) => {
+        setTimeout(res, milliseconds)
+    });
 }
