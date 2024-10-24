de8a24f84fc81758beabfa1001c4649e97d01eb6
Nicola Fiori (JD342) <nicola@jd342.net>
2024-10-24 21:05:02 +0100

    Solve

--- icpc/24q4/bapc2012p/annoying-mosquitos/solution.cc
+++ icpc/24q4/bapc2012p/annoying-mosquitos/solution.cc
@@ -11,49 +11,30 @@ void runCase() {
 
     int x[n];
     int y[n];
+    bool dead[n];
 
     for (int i = 0; i < n; i++) {
         cin >> x[i] >> y[i];
+        dead[i] = false;
     }
 
     int m;
 
     cin >> m;
 
-    int minCoord = -1000;
-    int maxCoord = 1000;
-    int coordinatesCount = abs(minCoord) + maxCoord + 1;
-
-    bool hit[coordinatesCount][coordinatesCount];
-
-    for (int i = 0; i < coordinatesCount; i++)
-        for (int j = 0; j < coordinatesCount; j++)
-            hit[i][j] = false;
+    int hits = 0;
 
     for (int j = 0; j < m; j++) {
         int ax, ay;
         cin >> ax >> ay;
-
-        for (int i = 0; i <= 50; i++) {
-            for (int ii = 0; ii <= 50; ii++) {
-                for (int k = -1; k <= 1; k += 2) {
-                    for (int kk = -1; kk <= 1; kk += 2) {
-                        int hitX = ax + (i * k);
-                        int hitY = ay + (ii * kk);
-                        if (hitX < -1000 || hitX > 1000) continue;
-                        if (hitY < -1000 || hitY > 1000) continue;
-                        hit[hitX + abs(minCoord)][hitY + abs(minCoord)] = true;
-                    }
-                }
+        for (int i = 0; i < n; i++) {
+            if (dead[i]) continue;
+            if (x[i] >= ax - 50 && x[i] <= ax + 50 &&
+                y[i] >= ay - 50 && y[i] <= ay + 50) {
+                hits++;
+                dead[i] = true;
             }
         }
-    }
-
-    int hits = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (hit[x[i] + abs(minCoord)][y[i] + abs(minCoord)])
-            hits++;
     }
 
     cout << hits << endl;
