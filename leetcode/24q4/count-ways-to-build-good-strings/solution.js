4657ab725a006d59ba1331f2b8a940ff3702a9e9
Nicola Fiori (JD342) <nicola@jd342.net>
2024-11-03 21:10:08 +0000

    First attempt (wrong)

--- leetcode/24q4/count-ways-to-build-good-strings/solution.js
+++ leetcode/24q4/count-ways-to-build-good-strings/solution.js
@@ -1,10 +1,20 @@
-/**
- * @param {number} low
- * @param {number} high
- * @param {number} zero
- * @param {number} one
- * @return {number}
- */
-var countGoodStrings = function(low, high, zero, one) {
+// Wrong
+function countGoodStrings(low, high, zero, one) {
+    let memo = new Map();
 
-};
+    return _count(0);
+
+    function _count(start) {
+        if (!memo.has(start)) memo.set(start, __count(start));
+        return memo.get(start);
+    }
+
+    function __count(start) {
+        if (start > high) return 0;
+        let count = 0;
+        if (start >= low) count++;
+        count += _count(start + zero);
+        count += _count(start + one);
+        return count;
+    }
+}
